d581f35b2da3d85f44c4988217e0e5e7baeed6d4
giwakunle <giwakunle@gmail.com>
2022-09-13 22:56:43 +0100

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+
+
+
+
+/**
+
+* main - prints _putchar, followed by a new line
+
+* Return: Always 0 (Success)
+
+*/
+
+int main(void)
+
+{
+
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u’);
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+
+
+return (0);
+
+}
+
